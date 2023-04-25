38a6632ba7585218bcc672ae25079f32a49015e4
Lachuuuu <108222128+Lachuuuu@users.noreply.github.com>
2023-04-25 10:44:27 +0200

    Update WorkshopsApplicationTests.java

--- src/test/java/com/example/workshops/WorkshopsApplicationTests.java
+++ src/test/java/com/example/workshops/WorkshopsApplicationTests.java
@@ -3,6 +3,8 @@ package com.example.workshops;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.Assert.assertTrue;
+
 @SpringBootTest
 class WorkshopsApplicationTests {
 

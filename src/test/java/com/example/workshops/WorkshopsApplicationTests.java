4e161333f49c3a1dfa1536a43829b7df176d8958
Lachuuuu <108222128+Lachuuuu@users.noreply.github.com>
2023-04-25 10:51:20 +0200

    asda

--- src/test/java/com/example/workshops/WorkshopsApplicationTests.java
+++ src/test/java/com/example/workshops/WorkshopsApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.workshops;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class WorkshopsApplicationTests {
